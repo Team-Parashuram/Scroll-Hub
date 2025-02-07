efa41fca951495675afb425aa3e99a764888ba32
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-02-08 03:48:49 +0530

    refactor: implement ImageKit authentication and integrate image upload provider

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
